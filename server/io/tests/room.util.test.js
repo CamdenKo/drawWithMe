3748d92947220007021e06312a5dc93157b75b05
Camden Ko <camdenko@gmail.com>
2017-11-20 11:41:57 -0600

    randomcharfromoptions test

--- server/io/tests/room.util.test.js
+++ server/io/tests/room.util.test.js
@@ -1,6 +1,25 @@
+const sinon = require('sinon')
+
+const {
+  randomCharFromOptions,
+} = require('../room.util')
 
 describe('room.util', () => {
   test('randomCharFromOptions', () => {
+    const possibleOptions = 'asdfjkl'.split('')
+    const randomStub = sinon.stub(Math, 'random')
+    randomStub.onFirstCall().returns(0)
+    randomStub.onSecondCall().returns(0.999999999999)
 
+    it('will return values in the range from the first to last', () => {
+      expect(randomCharFromOptions(possibleOptions)).toEqual('a')
+      expect(randomCharFromOptions(possibleOptions)).toEqual('l')
+    })
+    it('returns a string', () => {
+      expect(typeof randomCharFromOptions(possibleOptions)).toEqual('string')
+    })
+    it('returns string w/ length 1', () => {
+      expect(randomCharFromOptions(possibleOptions).length).toEqual(1)
+    })
   })
 })
