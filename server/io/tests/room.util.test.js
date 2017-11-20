32658ea8543f34a61ff6009e33f9b61d0961147e
Camden Ko <camdenko@gmail.com>
2017-11-20 11:45:54 -0600

    added babel packages

--- server/io/tests/room.util.test.js
+++ server/io/tests/room.util.test.js
@@ -5,11 +5,12 @@ const {
 } = require('../room.util')
 
 describe('room.util', () => {
-  test('randomCharFromOptions', () => {
+  describe('randomCharFromOptions', () => {
     const possibleOptions = 'asdfjkl'.split('')
     const randomStub = sinon.stub(Math, 'random')
     randomStub.onFirstCall().returns(0)
     randomStub.onSecondCall().returns(0.999999999999)
+    randomStub.returns(0)
 
     it('will return values in the range from the first to last', () => {
       expect(randomCharFromOptions(possibleOptions)).toEqual('a')
