87283729e5f103c3845272dfa14ec6c6fff08475
Camden Ko <camdenko@gmail.com>
2017-11-20 10:57:51 -0600

    basic scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  entry: ['babel-polyfill', './client/index.js'],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.svg$|\.ttf?|\.woff$|\.woff2|\.eof|\.eot/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        use: [
+          'url-loader',
+        ],
+      },
+    ],
+  },
+};
+
