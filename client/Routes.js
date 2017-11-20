18535afae8f88f965802e7f59f21cfca96762843
Camden Ko <camdenko@gmail.com>
2017-11-20 15:56:23 -0600

    basic routes

--- client/Routes.js
+++ client/Routes.js
@@ -1,2 +1,20 @@
 import React from 'react'
 import { Router } from 'react-router'
+import {
+  Route,
+  Switch,
+} from 'react-router-dom'
+
+import history from './history'
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router history={history}>
+        <div>LOADED</div>
+      </Router>
+    )
+  }
+}
+
+export default Routes
