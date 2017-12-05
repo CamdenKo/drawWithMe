fa0971d28dd45917c7191d376c1d4ccee52deb1a
Camden Ko <camdenko@gmail.com>
2017-12-05 09:55:22 -0600

    reducers plugged into redux

--- client/store/reducers/index.js
+++ client/store/reducers/index.js
@@ -2,10 +2,18 @@ import {
   combineReducers,
 } from 'redux'
 
+import chat from './chat/chat'
+import guesses from './guesses/guesses'
+import room from './room/room'
+import user from './user/user'
 import roomCode from './roomCode/roomCode'
 import word from './word/word'
 
 const reducer = combineReducers({
+  chat,
+  guesses,
+  room,
+  user,
   roomCode,
   word,
 })
@@ -13,3 +21,7 @@ const reducer = combineReducers({
 export default reducer
 export * from './roomCode/roomCode'
 export * from './word/word'
+export * from './chat/chat'
+export * from './guesses/guesses'
+export * from './room/room'
+export * from './user/user'
